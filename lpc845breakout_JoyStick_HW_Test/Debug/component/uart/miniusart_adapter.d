938e0dfb2e90d53a893c50acc7fb68c0e7219d82
ianlesni <ianlesni@gmail.com>
2023-07-14 15:58:31 -0300

    Agrego funcion para JS2

--- lpc845breakout_JoyStick_HW_Test/Debug/component/uart/miniusart_adapter.d
+++ lpc845breakout_JoyStick_HW_Test/Debug/component/uart/miniusart_adapter.d
@@ -1,30 +1,30 @@
 component/uart/miniusart_adapter.o component/uart/miniusart_adapter.d: \
  ../component/uart/miniusart_adapter.c \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_usart.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_usart.h \
  ../component/uart/uart.h
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_usart.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_usart.h:
 ../component/uart/uart.h:
--- lpc845breakout_JoyStick_HW_Test/Debug/utilities/fsl_debug_console.d
+++ lpc845breakout_JoyStick_HW_Test/Debug/utilities/fsl_debug_console.d
@@ -1,29 +1,29 @@
 utilities/fsl_debug_console.o utilities/fsl_debug_console.d: \
  ../utilities/fsl_debug_console.c ../utilities/fsl_debug_console.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h \
- C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\component\uart/uart.h
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h \
+ C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\component\uart/uart.h
 ../utilities/fsl_debug_console.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h:
-C\:\Users\Rprato\OneDrive\ -\ ADIMRA\ II\OneDrive\ -\ ADIMRA\Joystick\WS_Hardware_Test(doubt)\lpc845breakout_JoyStick_HW_Test\component\uart/uart.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/fsl_device_registers.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/core_cm0plus.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_version.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_compiler.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\CMSIS/cmsis_gcc.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/system_LPC845.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\device/LPC845_features.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_clock.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_common.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\drivers/fsl_reset.h:
+C\:\Users\Ilesnianski\OneDrive\ -\ ADIMRA\Joystick\WS_JS_Hardware_Test\lpc845breakout_JoyStick_HW_Test\component\uart/uart.h:
